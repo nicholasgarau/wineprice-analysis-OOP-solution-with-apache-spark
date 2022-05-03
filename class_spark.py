686bb233a1f9eb477060835bcfb0c0f39e33bdb0
Nicholas Garau <nicholasgarau@MacBook-Air-di-Nicholas.local>
2022-05-03 11:18:13 +0200

    updating main file and adding class_spark

--- class_spark.py
+++ class_spark.py
@@ -1,5 +1,6 @@
 import findspark
 from pyspark.sql import SparkSession
+from pyspark.sql.dataframe import DataFrame
 
 
 class SparkClass(object):
@@ -11,15 +12,45 @@ class SparkClass(object):
     methods:
     spark_start = a method to create a SparkSession
 
+    open_csv = a method that open a csv file as spark dataframe
+
     """
 
-    def __init__(self, path_config):
+    def __init__(self, path_config, filename):
         self.config = path_config
+        self.filename = filename
+        self.infile = self.open_csv()
 
-    def spark_start(self, app_name):
+    def spark_start(self, app_name=None):
         findspark.init(self.config)
         return SparkSession.builder.appName(app_name).getOrCreate()
 
+    def open_csv(self):
+        return self.spark_start(app_name='finalproj').read.csv(self.filename, sep=',', inferSchema=True, header=True)
 
 
+class DatasetCleaner(object):
+    """A class to clean a dataset from useless elements"""
 
+    def __init__(self, dataframe:DataFrame):
+        self.dataframe = dataframe
+
+    def dropnas(self):
+        """A method to drop none values"""
+        self.dataframe = self.dataframe.dropna()
+        return self
+
+    def dropper(self, useless_features):
+        """A method to drop useless features"""
+        self.dataframe = self.dataframe.drop(useless_features)
+        return self
+
+    def outliers_remove(self, feature, thresold):
+        """A method to remove the outliers """
+        self.dataframe = self.dataframe.filter(self.dataframe[feature] < thresold)
+        return self
+
+    def __call__(self):
+        return f'Dataset pulito.\nPrime 10 righe:\n{self.dataframe.show(7)}'
+
+    #todo: cerca di chiudere questa
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from pyspark.sql import SparkSession
-import findspark
-findspark.init('/usr/local/Cellar/apache-spark/3.2.1/libexec')
-spark = SparkSession.builder.appName('FinalProject').getOrCreate()
+import os
 
-df = spark.read.csv('white-wine-price-rating.csv', header=True, inferSchema=True)
+import class_spark
 
-df.show(4)
+'''Initializing spark session and importing file'''
+path = ''
+filename = 'white-wine-price-rating.csv'
+
+spark_home = '/usr/local/Cellar/apache-spark/3.2.1/libexec'
+df = class_spark.SparkClass(spark_home, filename).open_csv()
+df.show(5)
+
+'''Data cleaning and preprocessing'''
+
+useless_feat = 'FullName, VintageRating, VintageRatingCount, VintagePrice, VintageRatingPriceRatio, WineRatingPriceRatio'
+df_clean = class_spark.DatasetCleaner(df).dropper(useless_feat).dropnas()
+df_clean.s
